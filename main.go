a9d04ca8f3d1b1aee9db884a9a1158a2a2e2c98f
ygjken <ken.748322@gmail.com>
2021-06-19 01:04:00 +0900

    make react run in go server

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("./views/*.html") // html
-	r.Static("/src", "./views/src")  // react
-	r.GET("/", index)                // handler
+	r.LoadHTMLGlob("./views/build/*.html")        // html
+	r.Static("/static/", "./views/build/static/") // react
+	r.GET("/", index)                             // handler
 
 	r.Run(":8080")
 }
