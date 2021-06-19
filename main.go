fe5492575f44f44ac2ae27bd876e582624ab1b38
kugi-suke <kosuke.k.h91009@docomo.ne.jp>
2021-06-20 01:42:46 +0900

    divide branch for front

--- main.go
+++ main.go
@@ -11,11 +11,16 @@ func main() {
 
 	r.LoadHTMLGlob("./views/build/*.html")        // html
 	r.Static("/static/", "./views/build/static/") // react
-	r.GET("/", index)                             // handler
+	r.GET("/", index)
+	r.GET("/login", login)                             // handler
 
 	r.Run(":8080")
 }
 
 func index(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
+func login(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "login.html", gin.H{})
 }
