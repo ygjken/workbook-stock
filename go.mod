af5a3be0d77c4a643660147d1faba6253a4aad75
ygjken <ken.748322@gmail.com>
2021-10-08 01:01:03 +0900

    :construction:ログインミドルウェア作成中

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-contrib/sessions v0.0.3 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/koron/go-dproxy v1.3.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,11 +1,33 @@
 package middlewares
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-func LoginCheck(c *gin.Context) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
+type SessionInfo struct {
+	UserId interface{}
+}
+
+func LoginCheck(ctx *gin.Context) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var logininfo SessionInfo
+
+		session := sessions.Default(ctx)
+		logininfo.UserId = session.Get("UserId")
+
+		// セッションがない場合、ログインフォームをだす
+		if logininfo.UserId == nil {
+			log.Println("ログインしていません")
+			ctx.Redirect(http.StatusMovedPermanently, "/login")
+			ctx.Abort()
+		} else {
+			ctx.Set("UserId", logininfo.UserId)
+			ctx.Next()
+		}
+		log.Println("ログインチェック終わり")
 	}
 }
